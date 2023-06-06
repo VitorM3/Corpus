48cf75b02db72be3827d5ba8485ff37e2ca45d84
Vitor Loch Mafei Rosa <61475540+VitorM3@users.noreply.github.com>
2023-06-06 20:57:41 -0300

    FEAT: DDL base

--- database/code/DDL/database.sql
+++ database/code/DDL/database.sql
@@ -83,7 +83,7 @@ BEGIN
 END;
 /*============================================================================================*/
 /*Criar tabela de atendimentos*/
-CREATE TABLE attendances(
+CREATE TABLE attendance(
 id INT IDENTITY(1,1) PRIMARY KEY,
 pacient_id INT NOT NULL,
 doctor_id INT NOT NULL,
@@ -133,7 +133,102 @@ BEGIN
 	SELECT dbo.update_updated_at('room_permission_sector', u.id) FROM inserted u;
 END;
 /*==============================================================================================*/
+/*Criar Estoque*/
+CREATE TABLE inventory(
+id INT IDENTITY(1,1) PRIMARY KEY,
+name VARCHAR(50) NOT NULL UNIQUE,
+quantity TINYINT NOT NULL,
+created_at DATETIME DEFAULT GETDATE(),
+updated_at DATETIME DEFAULT GETDATE(),
+deleted_at DATETIME
+);
+CREATE TRIGGER tr_update_inventory_updated_at ON "inventory" AFTER UPDATE AS 
+BEGIN
+	SELECT dbo.update_updated_at('inventory', u.id) FROM inserted u;
+END;
+/*==============================================================================================*/
+/*Criar Encontro de um atendimento*/
+CREATE TABLE meeting(
+id INT IDENTITY(1,1) PRIMARY KEY,
+room_id INT NOT NULL,
+attendances_id INT NOT NULL,
+appointment DATETIME NOT NULL,
+started_at DATETIME,
+end_at DATETIME,
+created_at DATETIME DEFAULT GETDATE(),
+updated_at DATETIME DEFAULT GETDATE(),
+deleted_at DATETIME,
+CONSTRAINT fk_room_meeting FOREIGN KEY(room_id) REFERENCES room(id),
+CONSTRAINT fk_attendances_meeting FOREIGN KEY (attendances_id) REFERENCES attendances(id)
+);
 
+CREATE TRIGGER tr_update_meeting_updated_at ON "meeting" AFTER UPDATE AS 
+BEGIN
+	SELECT dbo.update_updated_at('meeting', u.id) FROM inserted u;
+END;
 
+/*==============================================================================================*/
+/*Criar Itens utilizados no encontro */
+CREATE TABLE meeting_tool(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    meeting_id INT NOT NULL,
+    tool_id INT NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
+    deleted_at DATETIME,
+    CONSTRAINT fk_meeting_meeting_tool FOREIGN KEY (meeting_id) REFERENCES meeting(id),
+    CONSTRAINT fk_tool_meeting_tool FOREIGN KEY (tool_id) REFERENCES inventory(id)
+)
 
+CREATE TRIGGER tr_update_meeting_tool_updated_at ON "meeting_tool" AFTER UPDATE AS 
+BEGIN
+	SELECT dbo.update_updated_at('meeting_tool', u.id) FROM inserted u;
+END;
+/*=============================================================================================*/
+/*Criar Exercicios*/
+CREATE TABLE exercise(
+id INT IDENTITY(1,1) PRIMARY KEY,
+name VARCHAR(50) NOT NULL UNIQUE,
+description VARCHAR(500) NOT NULL,
+knowledge_font VARCHAR(150) NOT NULL,
+created_at DATETIME DEFAULT GETDATE(),
+updated_at DATETIME DEFAULT GETDATE(),
+deleted_at DATETIME
+);
+CREATE TRIGGER tr_update_exercise_updated_at ON "exercise" AFTER UPDATE AS 
+BEGIN
+	SELECT dbo.update_updated_at('exercise', u.id) FROM inserted u;
+END;
+/*===========================================================================================*/
+/*Criar Itens utilizados em cada exercicio*/
+CREATE TABLE exercise_tool(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    exercise_id INT NOT NULL,
+    tool_id INT NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
+    deleted_at DATETIME,
+    CONSTRAINT fk_exercise_exercise_tool FOREIGN KEY (exercise_id) REFERENCES exercise(id),
+    CONSTRAINT fk_tool_exercise_tool FOREIGN KEY (tool_id) REFERENCES inventory(id)
+);
 
+CREATE TRIGGER tr_update_exercise_tool_updated_at ON "exercise_tool" AFTER UPDATE AS 
+BEGIN
+	SELECT dbo.update_updated_at('exercise_tool', u.id) FROM inserted u;
+END;
+/*===========================================================================================*/
+CREATE TABLE exercise_meeting(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    exercise_id INT NOT NULL,
+    meeting_id INT NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
+    deleted_at DATETIME,
+    CONSTRAINT fk_exercise_exercise_meeting FOREIGN KEY (exercise_id) REFERENCES exercise(id),
+    CONSTRAINT fk_meeting_exercise_meeting FOREIGN KEY (meeting_id) REFERENCES meeting(id),
+);
+CREATE TRIGGER tr_update_exercise_meeting_updated_at ON "exercise_meeting" AFTER UPDATE AS 
+BEGIN
+	SELECT dbo.update_updated_at('exercise_meeting', u.id) FROM inserted u;
+END;
+/*=========================================================================================*/

3e3a593fa7e2d28910d62666121c6ad7765dad35
Danilo Formanski <danilo16formanski@gmail.com>
2023-06-25 17:11:05 -0300

    FEAT: criado redirecionamento e inicializando o modal de agendar

--- corpus-front/src/core/components/Reports/ReportHeader.jsx
+++ corpus-front/src/core/components/Reports/ReportHeader.jsx
@@ -1,14 +1,27 @@
 import * as S from "./styles";
 import { Link } from "react-router-dom";
+// import ModalContext from "../Modal/ModalSchedule";
+// import { useContext } from "react";
 
 const ReportHeader = () => {
+  // const { openModal } = useContext(ModalContext);
+
+  // const OpenModalSchedule = () => {
+  //   openModal();
+  // };
+
   return (
     <S.WrapperHeader>
       <h1>Corpus</h1>
       <S.WrapperHeaderNavigation>
         <Link>Sobre</Link>
         <Link>Cadastros</Link>
-        <S.Button elementWidth="100%" elementheight="7vh" elementMargin="3vh">
+        <S.Button
+          // onClick={OpenModalSchedule}
+          elementWidth="100%"
+          elementheight="7vh"
+          elementMargin="3vh"
+        >
           Agendar consulta
         </S.Button>
       </S.WrapperHeaderNavigation>
--- corpus-front/src/router/Router.tsx
+++ corpus-front/src/router/Router.tsx
@@ -1,14 +1,20 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Login from "../pages/Login/Login";
 import RelatorioDiario from "../pages/RelatorioDiario/RelatorioDiario";
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-        <Route path="/" element={<Login/>} />
-        <Route path="/relatoriodiario" element={<RelatorioDiario/>} />
+      <Route path="/" element={<Navigate to="/login" replace={true} />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/relatoriodiario" element={<RelatorioDiario />} />
     </Route>
   )
-)
+);
 
 export default Router;

5e4f79dbd4085e90f72bfdc6d06cc45f67833f99
Vitor <vitorloch62@gmail.com>
2023-06-26 19:57:50 -0300

    FEAT: inventory

--- corpus-back/src/modules/inventory/routes/decorators/inventory.controller.decorator.ts
+++ corpus-back/src/modules/inventory/routes/decorators/inventory.controller.decorator.ts
@@ -1,7 +1,7 @@
 import { Controller, applyDecorators } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
-export default class ExerciseController {
+export default class InventoryController {
   public static default() {
     return applyDecorators(Controller('inventory'), ApiTags('Inventory'));
   }
